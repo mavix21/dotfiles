82d640de65886d215b7ff197c3f96b99bd0ff3f9
mavix21 <marcevizcarra2109@hotmail.com>
2023-07-25 20:08:18 -0500

    Ensure installed tsx parser

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -13,9 +13,10 @@ treesitter.setup({
 		"html",
 		"css",
 		"json",
+		"tsx",
 	},
 
-	-- Install parsers synchronously (onlu applied to 'ensure_installed') sync_install = false,
+	-- Install parsers synchronously (only applied to 'ensure_installed') sync_install = false,
 
 	-- Automatically install missing parsers when entering buffer
 	-- Recommendation: set to false if you don't have tree-sitter CLI installed locally
