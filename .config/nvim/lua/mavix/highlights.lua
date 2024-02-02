61ff09e07151841ee58f3b6ed0210b0256bdbb77
mavix21 <marcevizcarra2109@hotmail.com>
2024-02-01 21:39:36 -0500

    Feat: removing old nvim config

--- .config/nvim/lua/mavix/highlights.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "#21242B" })
--- .config/nvim/lua/mavix/init.lua
+++
@@ -1,6 +0,0 @@
-require("mavix.remap")
-require("mavix.packer")
-require("mavix.set")
-require("mavix.float")
-require("mavix.highlights")
-require("mavix.extras")
